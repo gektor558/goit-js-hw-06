fb1f8bc38f19373fd280be17b81d0af606bcc7d7
gektor558 <danil.trigub@gmail.com>
2023-08-31 16:56:24 +0200

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingridientsLists = document.querySelector('#ingredients');
+const ingredientsArr = [];
+ingredients.map(element => {
+  const ingingredientItem = document.createElement('li');
+  ingingredientItem.textContent = element;
+  ingingredientItem.classList.add('item');
+  ingredientsArr.push(ingingredientItem);
+});
+ingridientsLists.append(...ingredientsArr);
