2a5c6b2df8980fb1ef60c215050a3e0a0ce53af1
gektor558 <124904782+gektor558@users.noreply.github.com>
2023-08-31 14:56:46 +0000

    Deploying to gh-pages from @ gektor558/goit-js-hw-06@fb1f8bc38f19373fd280be17b81d0af606bcc7d7 🚀

---
+++ task-02.d5911d00.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#ingredients"),t=[];["Potatoes","Mushrooms","Garlic","Tomatos","Herbs","Condiments"].map((e=>{const o=document.createElement("li");o.textContent=e,o.classList.add("item"),t.push(o)})),e.append(...t);
+//# sourceMappingURL=task-02.d5911d00.js.map
