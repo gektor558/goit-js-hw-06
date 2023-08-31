e4d449430451212349e76918292f0117c8616e9b
gektor558 <124904782+gektor558@users.noreply.github.com>
2023-08-31 14:52:25 +0000

    Deploying to gh-pages from @ gektor558/goit-js-hw-06@6cc81983da5121ec3d84146389580df23942959d 🚀

---
+++ task-01.b83d38cf.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#categories"),l=e.children.length;console.log(`Number of categories: ${l}`),[...e.children].forEach((e=>{console.log(`Category: ${e.firstElementChild.textContent}`),console.log(`Elements: ${e.lastElementChild.children.length}`)}));
+//# sourceMappingURL=task-01.b83d38cf.js.map
